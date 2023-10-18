8abfd3744a3a3abaf85ca6636b01780f2c6bba15
ammar <ammar.ali.haidar.1990@gmail.com>
2023-10-18 18:10:22 +0400

    done

--- app/(routes)/(dashboard)/image/page.tsx
+++ app/(routes)/(dashboard)/image/page.tsx
@@ -42,7 +42,7 @@ import toast from "react-hot-toast";
 
 type Props = {};
 
-export const formSchema = z.object({
+ const formSchema = z.object({
   prompt: z.string().min(1, { message: "Prompt is required" }),
   amount:z.string().min(1),
   resolution:z.string().min(1)
